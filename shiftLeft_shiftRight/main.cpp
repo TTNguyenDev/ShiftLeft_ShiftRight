1cb20c4cb59a92a1dd3bf29ae6a4a5a4be8ddd37
TTNguyen <nttriet9a4@gmail.com>
2018-10-15 11:51:37 +0700

    Initial Commit

---
+++ shiftLeft_shiftRight/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  shiftLeft_shiftRight
+//
+//  Created by TT Nguyen on 10/15/18.
+//  Copyright © 2018 TT Nguyen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
