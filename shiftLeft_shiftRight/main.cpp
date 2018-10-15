d4c4f1c7a2f2613c803cc5a8aaae50937b51e26b
TTNguyen <nttriet9a4@gmail.com>
2018-10-15 13:33:04 +0700

    First Version

--- shiftLeft_shiftRight/main.cpp
+++ shiftLeft_shiftRight/main.cpp
@@ -1,15 +1,34 @@
-//
-//  main.cpp
-//  shiftLeft_shiftRight
-//
-//  Created by TT Nguyen on 10/15/18.
-//  Copyright © 2018 TT Nguyen. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+unsigned int mergeIPAddress(unsigned char ipNeedToMerge[4]) {
+    unsigned int newip;
+    newip = ipNeedToMerge[0];
+    
+    for (int i = 1; i < 4; i++) {
+        newip = newip << 8;
+        newip = newip | ipNeedToMerge[i];
+    }
+    return newip;
+}
+
+void rawIP(unsigned int ip, unsigned char ipAddress[4]) {
+    for (int i = 3; i >= 0; i--) {
+        ipAddress[i] = ip;
+        ip = ip >> 8;
+    }
+}
+
+int main() {
+    unsigned char ip[4] = {192, 168, 32, 111};
+    unsigned char ips[4];
+    
+    uint32_t mergedIP = mergeIPAddress(ip);
+    rawIP(mergedIP, ips);
+    
+    for (int i = 0; i < 4; i++) {
+        printf("%u\n", ips[i]);
+    }
     return 0;
 }
